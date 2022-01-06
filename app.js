9921b29fd034454834817f01e523ee920fd75c1e
SanyaOdare <sanyaodare@gmail.com>
2022-01-06 22:52:53 +0100

    clear alert

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ const searchUser = document.getElementById('searchUser');
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
 // Get input text
-const userText = e.target.value;
+  const userText = e.target.value;
 
-if(userText !== '') {
-  // Make http call
-  github.getUser(userText)
-  .then(data => {
-    if(data.profile.message === 'Not Found') {
-      // Show alert
-
-    } else {
-      // Show profile
-      ui.showProfile(data.profile);
-    }    
-  })  
-} else {
-  // Clear profile
-  // ui.clearProfile();
-}
+  if(userText !== '') {
+    // Make http call
+    github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }    
+    })  
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
 });
--- github.js
+++ github.js
@@ -1,7 +1,9 @@
 class GitHub {
   constructor() {
     this.client_id = "c2576757a073c2c0bc9b";
-    this.client_secret = "38560a3c42755e2284b8dc8c2a30de761f4a7fea";
+    this.client_secret = "4abea98e28daf90893fe3b98ab40313b67b16968";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
-    }
+      profile,
+      repos
+    };
   }
 }
